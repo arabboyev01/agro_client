08d825df871c67f932bbfd6468dddbf55da0a824
abbosbek <abbosarabboyev9@gmail.com>
2024-04-19 17:48:07 +0500

    main url changed

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 // export const MAIN_URL: string = "https://agro-server-elpt.onrender.com/api/v1" as string
-export const MAIN_URL: string = "http://185.196.213.182:3500/api/v1" as string
+export const MAIN_URL: string = "http://94.154.11.237:3500/api/v1" as string
+// export const MAIN_URL: string = "http://185.196.213.182:3500/api/v1" as string
 // export const MAIN_URL: string = "http://localhost:3500/api/v1" as string
