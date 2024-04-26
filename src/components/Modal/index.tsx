ea03083f7c30e355c6182fb7072bddc6c910aef7
abbosbek <abbosarabboyev9@gmail.com>
2024-04-26 17:17:31 +0500

    agro order chnages

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,6 +7,7 @@ import { MainButton } from '../Buttons/MainButton'
 import { ProductType } from '@/types'
 import { api } from '@/api'
 import { toast } from 'react-toastify'
+import { Language } from '@/hooks/language'
 
 interface ModalComponentProps {
     open: boolean
@@ -17,89 +18,115 @@ interface ModalComponentProps {
 const ModalComponent: FC<ModalComponentProps> = ({ open, setOpen, data }) => {
 
     const [loader, setLoader] = useState(false)
+    const { lang } = Language()
 
-    const onSubmit = (values: { name: string, phone: string}) => {
+    const onSubmit = (values: { name: string, phone: string }) => {
         setLoader(true)
         const payload = {
             plantName: data?.name,
-            plantVariety: 'none',    
+            plantVariety: 'none',
             price: data.price,
-            image: data.image,       
+            image: data.image,
             customerName: values.name,
             phone: values.phone,
             productId: data.id
         }
-        api.postData('orders', payload).then((data) => {
-            if (data.success) {
-                toast.success("Your order has sent!", {
+
+        if (values.name && values.phone) {
+            api.postData('orders', payload).then((data) => {
+                if (data.success) {
+                    toast.success("Your order has sent!", {
+                        theme: "dark"
+                    })
+                    setOpen(false)
+                    setLoader(false)
+                } else {
+                    toast.error(data.message, {
+                        theme: "dark"
+                    })
+                    setLoader(false)
+                }
+            }).catch((err: any) => {
+                toast.error(err.message, {
                     theme: "dark"
                 })
-                setOpen(false)
                 setLoader(false)
-            }else {
-                toast.error(data.message, {
-                    theme: "dark"
-                })
-                setLoader(false)
-            }
-        }).catch((err: any) => {
-            toast.error(err.message, {
-                theme: "dark"
             })
-            setLoader(false)
-        })
+        }
     }
 
-    return(
-    <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        hideBackdrop={true}
-    >
-        <S.ModalComponentContent>
-            <S.HeaderModal>
-                <S.TextWrapper>
-                    <S.ContactName>Контактные данные</S.ContactName>
-                    <S.ModalDesc>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard</S.ModalDesc>
-                </S.TextWrapper>
+    const initialValues = {
+        name: "",
+        phone: "+998"
+    };
 
-                <S.CloseIcon onClick={() => setOpen(false)}>
-                    <CloseSVG />
-                </S.CloseIcon>
-            </S.HeaderModal>
-            <Form
-                onSubmit={onSubmit}
-                render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
-                        <S.Inputs>
-                            <S.InputForms>
-                                <S.InputName>Имя:</S.InputName>
-                                <Field name="name">
-                                {({ input, meta }) =>(
-                                    <S.Input {...input}/>
-                                )}
-                                </Field>
-                            </S.InputForms>
-                            <S.InputForms>
-                                <S.InputName>Hомер телефона:</S.InputName>
-                                <Field name="phone">
-                                {({ input, meta }) =>(
-                                    <S.Input {...input} type='number' />
-                                )}
-                                </Field>
-                            </S.InputForms>
-                            <S.ButtonWrapper>
-                                <MainButton width={200} height={42.55} text='Отправить' textSize={14} loader={loader}/>
-                                <S.CallButton>Позвонить</S.CallButton>
-                            </S.ButtonWrapper>
-                        </S.Inputs>
-                    </form>
-                )}
-            />
+    const validate = (values: { name: string, phone: string }) => {
+        const errors: { name?: string; phone?: string } = {}; 
 
-        </S.ModalComponentContent>
-    </Modal>
-)}
+        if(!values.name){
+            errors.name = "required"
+        }
+
+        if(!values.phone){
+            errors.phone = "required"
+        }
+
+        return errors
+    }
+
+    return (
+        <Modal
+            open={open}
+            onClose={() => setOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            hideBackdrop={true}
+        >
+            <S.ModalComponentContent>
+                <S.HeaderModal>
+                    <S.TextWrapper>
+                        <S.ContactName>{lang('order.title')}</S.ContactName>
+                        <S.ModalDesc>{lang('order.desc')}</S.ModalDesc>
+                    </S.TextWrapper>
+
+                    <S.CloseIcon onClick={() => setOpen(false)}>
+                        <CloseSVG />
+                    </S.CloseIcon>
+                </S.HeaderModal>
+                <Form
+                    onSubmit={onSubmit}
+                    validate={validate}
+                    initialValues={initialValues}
+                    render={({ handleSubmit }) => (
+                        <form onSubmit={handleSubmit}>
+                            <S.Inputs>
+                                <S.InputForms>
+                                    <S.InputName>{lang('order.name')}:</S.InputName>
+                                    <Field name="name">
+                                        {({ input }) => (
+                                            <S.Input {...input} placeholder={lang('order.full_name')}/>
+                                        )}
+                                    </Field>
+                                </S.InputForms>
+                                <S.InputForms>
+                                    <S.InputName>{lang('order.phone')}:</S.InputName>
+                                    <Field name="phone">
+                                        {({ input, meta }) => (
+                                            <S.Input {...input} type='tel' placeholder={lang('order.phone_number')}/>
+                                        )}
+                                    </Field>
+                                </S.InputForms>
+                                <S.ButtonWrapper>
+                                    <MainButton width={200} height={42.55} text={lang('order.send')} textSize={14} loader={loader} />
+                                    <S.CallButton onClick={() => setOpen(false)}>{lang('order.cancel')}</S.CallButton>
+                                </S.ButtonWrapper>
+                            </S.Inputs>
+                        </form>
+                    )}
+                />
+
+            </S.ModalComponentContent>
+        </Modal>
+    )
+}
 export default ModalComponent
