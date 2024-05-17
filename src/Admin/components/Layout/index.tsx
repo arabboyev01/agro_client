aac755f8e3e55709edf998f04731576b298c32df
arabboev <abbosarabboyev9@gamil.com>
2024-05-17 14:56:37 +0500

    some changes

--- src/Admin/components/Layout/index.tsx
+++ src/Admin/components/Layout/index.tsx
@@ -1,11 +1,32 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactNode, useCallback, useLayoutEffect, useState } from 'react'
 import { CContainer } from '@coreui/react'
 import SIdebarComponent from '../Sideabar'
 import Navbar from '../Navbar'
+import { api } from '@/api'
 
 const AdminLayout: FC<{ children: ReactNode }> = ({ children }) => {
+
+  const [isAdmin, setIsAdmin] = useState(true)
+
+  const handleRoute = useCallback(() => {
+    api.authGet('user').then(data => {
+      if (data?.success && data.user.role === "ADMIN") {
+        setIsAdmin(true)
+      } else if(data?.success && data.user.role === "USER") {
+        setIsAdmin(false)
+      }
+    }).catch(err => console.log(err))
+  }, [])
+
+  useLayoutEffect(() => {
+    handleRoute()
+  }, [handleRoute])
+
   return (
     <div className="c-app" style={{ position: "fixed", width: "100%", height: "100vh" }}>
+      {!isAdmin && <div className="container">
+        <h1 className="text_title">You do not have permission</h1>
+      </div>}
       <div className="c-wrapper">
         <div className="c-sidebar" id="sidebar">
           <SIdebarComponent />
