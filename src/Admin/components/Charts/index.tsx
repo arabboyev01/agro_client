f96737a6f82cd6782336ba29a9eb3e4ec1f2d7ec
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2024-05-30 14:12:52 +0500

    language changed

--- src/Admin/components/Charts/index.tsx
+++ src/Admin/components/Charts/index.tsx
@@ -1,39 +1,52 @@
-import { CChart } from '@coreui/react-chartjs'
+import { Language } from "@/hooks/language";
+import { CChart } from "@coreui/react-chartjs";
 
 const CHartComponent = () => {
-    return (
-        <CChart
-            type="bar"
-            style={{ height: "700px", width: "100%" }}
-            data={{
-                labels: ['Products', 'Orders', 'Soils', 'Sales', 'Map'],
-                datasets: [
-                    {
-                        label: "May statistics",
-                        data: [11, 16, 7, 3, 14],
-                        backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB'],
-                    },
-                ],
-            }
-            }
-            options={{
-                plugins: {
-                    legend: {
-                        labels: {
-                            color: "grey",
-                        }
-                    }
-                },
-                scales: {
-                    r: {
-                        grid: {
-                            color: "grey",
-                        },
-                    }
-                }
-            }}
-        />
-    )
-}
+  const { lang } = Language("dashboard");
+  return (
+    <CChart
+      type="bar"
+      style={{ height: "700px", width: "100%" }}
+      data={{
+        labels: [
+          `${lang("products")}`,
+          `${lang("orders")}`,
+          `${lang("soils")}`,
+          `${lang("sails")}`,
+          `${lang("map")}`,
+        ],
+        datasets: [
+          {
+            label: lang("statistics"),
+            data: [11, 16, 7, 3, 14],
+            backgroundColor: [
+              "#FF6384",
+              "#4BC0C0",
+              "#FFCE56",
+              "#E7E9ED",
+              "#36A2EB",
+            ],
+          },
+        ],
+      }}
+      options={{
+        plugins: {
+          legend: {
+            labels: {
+              color: "grey",
+            },
+          },
+        },
+        scales: {
+          r: {
+            grid: {
+              color: "grey",
+            },
+          },
+        },
+      }}
+    />
+  );
+};
 
-export default CHartComponent
+export default CHartComponent;
--- src/Admin/components/Products/DumbProducts.tsx
+++ src/Admin/components/Products/DumbProducts.tsx
@@ -1,14 +1,26 @@
-import { FC } from "react"
-import ComponentHeader from "../ComponentHeader"
-import * as S from "./style.products"
-import TableComponent from "../Table"
-import { PlantTypeProp, ProductsType } from "@/Admin/types"
-import { dataSet, plantsTypeColumns } from "./data"
+import { FC } from "react";
+import ComponentHeader from "../ComponentHeader";
+import * as S from "./style.products";
+import TableComponent from "../Table";
+import { PlantTypeProp, ProductsType } from "@/Admin/types";
+import { dataSet, plantsTypeColumns } from "./data";
 
-const DumbProducts: FC<PlantTypeProp<ProductsType>> = ({ navigate, navigateTo, data }) => (
-    <S.StyledProducts>
-        <ComponentHeader title="Add Product" onClick={() => navigate && navigate("/admin/products/add")} />
-        <TableComponent user={dataSet(data)} navigateTo={navigateTo} column={plantsTypeColumns}/>
-    </S.StyledProducts>
-)
-export default DumbProducts
+const DumbProducts: FC<PlantTypeProp<ProductsType>> = ({
+  navigate,
+  navigateTo,
+  data,
+  lang,
+}) => (
+  <S.StyledProducts>
+    <ComponentHeader
+      title={lang("add")}
+      onClick={() => navigate && navigate("/admin/products/add")}
+    />
+    <TableComponent
+      user={dataSet(data)}
+      navigateTo={navigateTo}
+      column={plantsTypeColumns}
+    />
+  </S.StyledProducts>
+);
+export default DumbProducts;
