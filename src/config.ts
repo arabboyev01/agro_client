8e50061ef7e8ed11645d48fa973b6e7cf5155f5b
root <root@spaceargo.uz>
2024-08-05 05:37:38 +0000

    changed

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const MAIN_URL: string = "http://localhost:3500/api/v1" as string
-export const MAIN_URL: string = "http://94.154.11.237:3500/api/v1" as string
+export const MAIN_URL: string = "http://94.154.11.237:8000/api/v1" as string
